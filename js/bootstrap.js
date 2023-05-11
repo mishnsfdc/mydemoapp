2e044b31f644c462bd7b0543e842859664ed87ff
Namita Mishra <namita.mishra@namitam-ltmw8uk.internal.salesforce.com>
2023-05-11 13:12:19 +0530

    My Second Code

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -18,6 +18,9 @@ if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery"
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
+ * 
+ * 
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
